fe1d2534f9f29df70d7fd38a65bd3fd08888da68
gon <opve_5@naver.com>
2020-09-05 20:00:00 +0900

    Firebase and app link. Sign up and Sign in

--- app/src/main/java/com/example/making_f/LoginActivity.kt
+++ app/src/main/java/com/example/making_f/LoginActivity.kt
@@ -3,21 +3,48 @@ package com.example.making_f
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    var auth : FirebaseAuth? = null // 권한 라이브러리 불러오기 [코드 만들기 위한 작업]
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        auth = FirebaseAuth.getInstance() // 회원 가입 코드
         btn_login.setOnClickListener {
-            Toast.makeText(this, "로그인 성공 화면", Toast.LENGTH_LONG).show()
+            signinEmail()
         }
 
-        btn_signin_signup.setOnClickListener {
-            val nextIntent = Intent(this, SignUpActivity::class.java)
-            startActivity(nextIntent)
+        btn_signin_signup.setOnClickListener{
+            startActivity(Intent(this,SignUpActivity::class.java))
+            finish()
+        }
+    }
+
+    fun signinEmail(){ //로그인 한 코드
+        auth?.signInWithEmailAndPassword(edit_signin_email.text.toString(),edit_siginin_password.text.toString())
+            ?.addOnCompleteListener {
+                    task ->
+                if(task.isSuccessful){
+                    //아이디와 패스워드가 맞았을 경우 나타나는 부분
+                    moveMainPage(task.result?.user) //id 생성이 성공적으로 만들어졌을경우
+                }else{
+                    //틀렸거나 없을경우!
+                    Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
+                    Log.e("LoginFail","Login Fail")
+                }
+            }
+    }
+
+    fun moveMainPage(user:FirebaseUser?) {//로그인 성공 시 다음페이지로 가는 부분.
+        if(user != null) {
+            startActivity(Intent(this,MainActivity::class.java))
         }
     }
 }
--- app/src/main/java/com/example/making_f/SignUpActivity.kt
+++ app/src/main/java/com/example/making_f/SignUpActivity.kt
@@ -3,17 +3,57 @@ package com.example.making_f
 import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignUpActivity : AppCompatActivity() {
+
+    var auth : FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
-
+        auth = FirebaseAuth.getInstance() // 회원 가입 코드
         btn_signup.setOnClickListener {
-            val nextIntent = Intent(this, SchoolCertActivity::class.java)
-            startActivity(nextIntent)
+            if(edit_signup_name.text.toString()==""){
+                Toast.makeText(this,"이름을 입력하세요.",Toast.LENGTH_SHORT).show()
+            }else if(!android.util.Patterns.EMAIL_ADDRESS.matcher(edit_signup_email.text.toString()).matches()){
+                Toast.makeText(this,"이메일 형식을 올바르게 입력해주세요.",Toast.LENGTH_SHORT).show()
+            }else if((edit_signup_password.text.toString() != edit_signup_password_confirm.text.toString()) || edit_signup_password.text.toString().length<6){
+                Toast.makeText(this,"비밀번호는 6글자 이상 이고 비밀번호 확인과 동일해야 합니다.",Toast.LENGTH_SHORT).show()
+            }else{
+                Signup()
+            }
+        }
+
+        btn_find_account.setOnClickListener {
+            startActivity(Intent(this,LoginActivity::class.java))
+            finish()
+        }
+
+    }
+
+    fun Signup() { //회원가입 코드를 받아오기 위한 작업
+        auth?.createUserWithEmailAndPassword(edit_signup_email.text.toString(),edit_signup_password.text.toString())
+            ?.addOnCompleteListener {
+                    task ->
+                if(task.isSuccessful){
+                    //회원가입
+                    moveMainPage(task.result?.user) //id 생성이 성공적으로 만들어졌을경우 MainPage로 이동
+                }else{
+                    //가입 안되면 에러메세지
+                    Toast.makeText(this,"회원가입 실패",Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    fun moveMainPage(user: FirebaseUser?) {//로그인 성공 시 다음페이지로 가는 부분.
+        if(user != null) {
+            startActivity(Intent(this,MainActivity::class.java))
         }
     }
 }
