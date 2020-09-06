d0ae0a9982a96823cf0f34934a3658e30c095da0
gon <opve_5@naver.com>
2020-09-06 11:56:45 +0900

    LoginPersistence and Logout A part ending

--- app/src/main/java/com/example/making_f/MainActivity.kt
+++ app/src/main/java/com/example/making_f/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.making_f
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.sign
 
 class MainActivity : AppCompatActivity() {
+    var auth : FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = FirebaseAuth.getInstance() // 회원 가입 코드
+
+        btn_logout.setOnClickListener {
+            signout()
+            startActivity(Intent(this,LoginActivity::class.java))
+        }
+    }
+
+    private fun signout() {
+        FirebaseAuth.getInstance().signOut();
     }
 }
