ea9340aeaf788f8daecc883ba352483272525af2
조영진 <7daydreams@naver.com>
2020-09-24 00:47:34 +0900

    add XML files

---
+++ app/src/main/java/com/example/making_f/navigation/ProfileEditActivity.kt
@@ -0,0 +1,13 @@
+package com.example.making_f.navigation
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.making_f.R
+
+class ProfileEditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_edit)
+    }
+}
--- app/src/main/java/com/example/making_f/navigation/UserFragment.kt
+++ app/src/main/java/com/example/making_f/navigation/UserFragment.kt
@@ -1,11 +1,15 @@
 package com.example.making_f.navigation
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.making_f.R
+import kotlinx.android.synthetic.main.fragment_user.*
+import kotlinx.android.synthetic.main.fragment_user.view.*
 
 class UserFragment : Fragment() {
     override fun onCreateView(
@@ -14,6 +18,12 @@ class UserFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_user, container, false)
+
+        view.mypage_imgbutton.setOnClickListener { view ->
+            Log.d("mypage_imgbutton", "Selected")
+            startActivity(Intent(getActivity()!!, ProfileEditActivity::class.java))
+        }
+
         return view
     }
 }
