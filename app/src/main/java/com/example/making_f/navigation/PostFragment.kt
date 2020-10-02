232019cf776fe9054742b2a57bac2c09b5433e28
gon <opve_5@naver.com>
2020-10-03 04:13:10 +0900

    detail page exit

--- app/src/main/java/com/example/making_f/navigation/PostFragment.kt
+++ app/src/main/java/com/example/making_f/navigation/PostFragment.kt
@@ -7,19 +7,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.making_f.Activity.AddPhotoActivity
 import com.example.making_f.Activity.ProfileEditActivity
 import com.example.making_f.R
+import com.example.making_f.navigation.model.ContentDTO
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.QuerySnapshot
 import kotlinx.android.synthetic.main.fragment_post.*
 import kotlinx.android.synthetic.main.fragment_post.view.*
+import kotlinx.android.synthetic.main.item_detail.view.*
 
 class PostFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    var firestore : FirebaseFirestore? = null
+    override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_post, container, false)
+        firestore = FirebaseFirestore.getInstance()
+
+        view.detailviewfragment_recyclerview.adapter = DetailVeiwRecyclerViewAdapter()
+        view.detailviewfragment_recyclerview.layoutManager = LinearLayoutManager(activity)
 
         view.board_write.setOnClickListener {
             Log.d("board_write move", "board_write move")
@@ -29,4 +38,51 @@ class PostFragment : Fragment() {
         }
         return view
     }
+    inner class DetailVeiwRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        var contentDTOs : ArrayList<ContentDTO> = arrayListOf()
+        var contentUidList : ArrayList<String> = arrayListOf()
+
+        init {
+            firestore?.collection("images")?.orderBy("timestamp")?.addSnapshotListener { querySnapshot: QuerySnapshot?, firebaseFirestoreException: FirebaseFirestoreException? ->
+                contentDTOs.clear()
+                contentUidList.clear()
+                for(snapshot in querySnapshot!!.documents){
+                    var item = snapshot.toObject(ContentDTO::class.java)
+                    contentDTOs.add(item!!)
+                    contentUidList.add(snapshot.id)
+                }
+                notifyDataSetChanged()
+            }
+        }
+
+
+        override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
+            var view = LayoutInflater.from(p0.context).inflate(R.layout.item_detail,p0,false)
+            return CustomViewHolder(view)
+        }
+
+        inner class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+        override fun getItemCount(): Int {
+            return contentDTOs.size
+        }
+
+        override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
+            var viewholder = (p0 as CustomViewHolder).itemView
+
+            //UserId
+            viewholder.detailviewitem_profile_textview.text = contentDTOs!![p1].userId
+
+            //Image
+            Glide.with(p0.itemView.context).load(contentDTOs!![p1].imageUrl).into(viewholder.detailviewitem_imageView_content)
+
+            //Explain of content
+            viewholder.detailviewitem_explain_textview.text = contentDTOs!![p1].explain
+
+            //likes
+            viewholder.detailviewitem_favoritecounter_textview.text = "Likes " + contentDTOs!![p1].favoriteCount
+        }
+
+    }
+
 }
