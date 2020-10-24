f25c02322c3d4da360a3f49347d0e54fd919bbff
조영진 <7daydreams@naver.com>
2020-10-24 16:56:45 +0900

    tflite test success

--- app/src/main/java/com/example/making_f/navigation/TestActivity.kt
+++ app/src/main/java/com/example/making_f/navigation/TestActivity.kt
@@ -2,29 +2,35 @@ package com.example.making_f.navigation
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import android.widget.SeekBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.making_f.R
 import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions
 import com.google.firebase.ml.common.modeldownload.FirebaseModelManager
-import com.google.firebase.ml.custom.FirebaseCustomLocalModel
-import com.google.firebase.ml.custom.FirebaseCustomRemoteModel
-import com.google.firebase.ml.custom.FirebaseModelInterpreter
-import com.google.firebase.ml.custom.FirebaseModelInterpreterOptions
+import com.google.firebase.ml.custom.*
 import kotlinx.android.synthetic.main.activity_test.*
+import java.util.*
 
 class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
 
+        val remoteModel = FirebaseCustomRemoteModel.Builder("softmax").build()
+
+        val conditions = FirebaseModelDownloadConditions.Builder()
+            .requireWifi()
+            .build()
+        FirebaseModelManager.getInstance().download(remoteModel, conditions)
+            .addOnCompleteListener {
+                //success
+            }
+
         val localModel = FirebaseCustomLocalModel.Builder()
             .setAssetFilePath("model.tflite")
             .build()
-
-        val options = FirebaseModelInterpreterOptions.Builder(localModel).build()
-        val interpreter = FirebaseModelInterpreter.getInstance(options)
 
         var sek1: Int
         var sek2 = 0
@@ -33,6 +39,7 @@ class TestActivity : AppCompatActivity() {
         var sek5 = 0
         var sek6 = 0
         var sek7 = 0
+        var input = arrayOf(0,0,0,0,0,0,0)
 
         btn_popup_result.setOnClickListener { view ->
             sek1 = seek1.progress
@@ -42,7 +49,48 @@ class TestActivity : AppCompatActivity() {
             sek5 = seek5.progress
             sek6 = seek6.progress
             sek7 = seek7.progress
-            Toast.makeText(this, "${sek1}, ${sek2}, ${sek3}, ${sek4}, ${sek5}, ${sek6}, ${sek7}", Toast.LENGTH_SHORT).show()
+
+            val input = Array(1){FloatArray(7)}
+
+            input[0][0] = sek1.toFloat()
+            input[0][1] = sek2.toFloat()
+            input[0][2] = sek3.toFloat()
+            input[0][3] = sek4.toFloat()
+            input[0][4] = sek5.toFloat()
+            input[0][5] = sek6.toFloat()
+            input[0][6] = sek7.toFloat()
+
+            val options = FirebaseModelInterpreterOptions.Builder(localModel).build()
+
+            val interpreter = FirebaseModelInterpreter.getInstance(options)
+            val inputOutputOptions = FirebaseModelInputOutputOptions.Builder()
+                .setInputFormat(0, FirebaseModelDataType.FLOAT32, intArrayOf(1, 7))
+                .setOutputFormat(0, FirebaseModelDataType.FLOAT32, intArrayOf(1, 5))
+                .build()
+            val inputs = FirebaseModelInputs.Builder()
+                .add(input)
+                .build()
+            interpreter?.run(inputs, inputOutputOptions)
+                ?.addOnSuccessListener { result ->
+                    var output = result.getOutput<Array<FloatArray>>(0)
+                    val probabilities = output[0]
+                    var bestMatch = 0f
+                    var bestMatchIndex = 0
+                    for (i in probabilities.indices){
+                        if(probabilities[i]>bestMatch){
+                            bestMatch = probabilities[i]
+                            bestMatchIndex = i
+                        }
+                    }
+
+                    var tmp = Arrays.toString(output[0])
+                    Log.d("HUSKY2", "${output}")
+                    //Toast.makeText(this, "${tmp}", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "${tmp}, ${bestMatchIndex}", Toast.LENGTH_LONG).show()
+                }
+                ?.addOnFailureListener {e ->
+                    Log.d("HUSKY2", "GGWP :(${e.toString()}")
+                }
         }
     }
 }
