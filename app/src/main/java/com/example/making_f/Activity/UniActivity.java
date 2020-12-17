866d2febe7e917eb67d6daf3ccf052c85060378c
gon <opve555@gmail.com>
2020-12-18 02:58:03 +0900

    hackerTon for public data is ended.

--- app/src/main/java/com/example/making_f/Activity/UniActivity.java
+++ app/src/main/java/com/example/making_f/Activity/UniActivity.java
@@ -1,12 +1,14 @@
 package com.example.making_f.Activity;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,7 +32,8 @@ public class UniActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private LinearLayoutManager linearLayoutManager;
 
-    String[] items = {"대학구분","지역","학교","학과"};
+    String searchOption;
+    String[] items = {"대학구분", "지역", "학교", "학과"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,31 +41,42 @@ public class UniActivity extends AppCompatActivity {
         setContentView(R.layout.activity_university);
 
         Spinner spinner = findViewById(R.id.spinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,items);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+                searchOption = items[position];
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
             }
         });
 
+        Button btn_uni = (Button) findViewById(R.id.UniSearch);
+        final EditText searchWord = (EditText)findViewById(R.id.searchWord);
+        btn_uni.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                search(searchWord.getText().toString(),searchOption);
+            }
+        });
+
+
         readWeatherData();
 
-        recyclerView = (RecyclerView)findViewById(R.id.universityActivity_recyclerview);
+        recyclerView = (RecyclerView) findViewById(R.id.universityActivity_recyclerview);
         linearLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(linearLayoutManager);
         uniAdapter = new UniAdapter(UniSamples);
         recyclerView.setAdapter(uniAdapter);
 
+
     }
+
 
     private void readWeatherData() {
         InputStream is = getResources().openRawResource(R.raw.data);
@@ -92,6 +106,62 @@ public class UniActivity extends AppCompatActivity {
         } catch(IOException e){
             Log.wtf("MyActivity","Error reading data file on line" + line, e);
             e.printStackTrace();
+        }
+    }
+
+    private void search(String searchWord, String searchOption){
+        UniSamples.clear();
+
+        if(searchOption.equals("대학구분")){
+            readWeatherData();
+            for(int i=0;i<UniSamples.size();i++){
+                if(!searchWord.equals(UniSamples.get(i).getUniSeparators())){
+                    UniSamples.remove(i);
+                    i--;
+                }
+            }
+            linearLayoutManager = new LinearLayoutManager(this);
+            recyclerView.setLayoutManager(linearLayoutManager);
+            uniAdapter = new UniAdapter(UniSamples);
+            recyclerView.setAdapter(uniAdapter);
+        }else if(searchOption.equals("지역")){
+            readWeatherData();
+            for(int i=0;i<UniSamples.size();i++){
+                if(!searchWord.equals(UniSamples.get(i).getLocation())){
+                    UniSamples.remove(i);
+                    i--;
+                }
+            }
+            linearLayoutManager = new LinearLayoutManager(this);
+            recyclerView.setLayoutManager(linearLayoutManager);
+            uniAdapter = new UniAdapter(UniSamples);
+            recyclerView.setAdapter(uniAdapter);
+
+        }else if(searchOption.equals("학교")){
+            readWeatherData();
+            for(int i=0;i<UniSamples.size();i++){
+                if(!searchWord.equals(UniSamples.get(i).getUniversity())){
+                    UniSamples.remove(i);
+                    i--;
+                }
+            }
+            linearLayoutManager = new LinearLayoutManager(this);
+            recyclerView.setLayoutManager(linearLayoutManager);
+            uniAdapter = new UniAdapter(UniSamples);
+            recyclerView.setAdapter(uniAdapter);
+
+        }else if(searchOption.equals("학과")){
+            readWeatherData();
+            for(int i=0;i<UniSamples.size();i++){
+                if(!searchWord.equals(UniSamples.get(i).getDepartment())){
+                    UniSamples.remove(i);
+                    i--;
+                }
+            }
+            linearLayoutManager = new LinearLayoutManager(this);
+            recyclerView.setLayoutManager(linearLayoutManager);
+            uniAdapter = new UniAdapter(UniSamples);
+            recyclerView.setAdapter(uniAdapter);
         }
     }
 }
